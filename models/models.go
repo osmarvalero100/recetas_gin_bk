a7cf000e87113c7bc8f05525c257cb71c023b23a
Osmar Valero <osmarvalero100@gmail.com>
2025-06-11 21:04:44 -0500

    Add Recipes model

--- models/models.go
+++ models/models.go
@@ -1,6 +1,9 @@
 package models
 
-import "backend/database"
+import (
+	"backend/database"
+	"time"
+)
 
 type Category struct {
 	ID   uint   `gorm:"primaryKey" json:"id"`
@@ -10,7 +13,19 @@ type Category struct {
 
 type categories []Category
 
+type Recipes struct {
+	ID          uint      `gorm:"primaryKey" json:"id"`
+	CategoryID  uint      `gorm:"not null" json:"category_id"`
+	Category    Category  `gorm:"foreignKey:CategoryID" json:"category"`
+	Name        string    `gorm:"type:varchar(100);not null" json:"name"`
+	slug        string    `gorm:"type:varchar(100);not null;unique" json:"slug"`
+	Time        string    `gorm:"type:varchar(100);not null" json:"time"`
+	Photo       string    `gorm:"type:varchar(255);not null" json:"photo"`
+	Description string    `gorm:"type:text;not null" json:"description"`
+	DateAdd     time.Time `json:"date_add"`
+}
+
 func Migrations() {
 	db := database.Database()
-	db.AutoMigrate(&Category{})
+	db.AutoMigrate(&Category{}, &Recipes{})
 }
